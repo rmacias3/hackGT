613166a54121a5cb017399556bc2d12dd134a44f
sridevik <koushiksridevi8@gmail.com>
2015-09-26 20:38:55 -0400

    small changes

--- make_azure_webservice_call.py
+++ make_azure_webservice_call.py
@@ -4,23 +4,27 @@ import urllib2
 import json 
 
 
+
 data =  {
 
         "Inputs": {
 
                 "input1":
                 {
-                    "ColumnNames": ["category", "month", "zipcode", "label"],
-                    "Values": [ [ "clothing", "jan", "30318", "10" ], [ "clothing", "jan", "30318", "0" ], ]
+                    "ColumnNames": ["casuals", "jackets", "shoes", "formals", "zip_code", "month", "sales"],
+                    "Values": [ [ 1, 1, 0,1,"30318", 4, "10" ], [ 1, 1, 0,1,"30318", 4, "10" ], ]
                 },        },
             "GlobalParameters": {
 }
     }
 
+
 body = str.encode(json.dumps(data))
 
-url = 'https://ussouthcentral.services.azureml.net/workspaces/7f29d25c24934d8d93a67b615ad2714a/services/c74654ca8d0a42ed9d4a6b21f066b25f/execute?api-version=2.0&details=true'
-api_key = 'T0WtfiEngJLEZWiw55xTkw+JzxT+u8EriXmj1VC3/TfXBXI49JxhQ/kl2e2+Mrow0zjmsmI7IcJC6SFX1rCZtw==' # Replace this with the API key for the web service
+url = 'https://ussouthcentral.services.azureml.net/workspaces/7f29d25c24934d8d93a67b615ad2714a/services/28f9eb3825f448eaa9bfe10a4ffbf888/execute?api-version=2.0&details=true'
+# api_key = 'T0WtfiEngJLEZWiw55xTkw+JzxT+u8EriXmj1VC3/TfXBXI49JxhQ/kl2e2+Mrow0zjmsmI7IcJC6SFX1rCZtw==' # Replace this with the API key for the web service
+# api_key= 'nM31V5EgVJ7j2GAqDmTzNlbjlYiqkb8/kZLqjwsL6H6LzSmJqbbdXNel9SxR8oGkhYnb2ENMNj5Iuy1KZrIJWQ=='
+api_key = 'nM31V5EgVJ7j2GAqDmTzNlbjlYiqkb8/kZLqjwsL6H6LzSmJqbbdXNel9SxR8oGkhYnb2ENMNj5Iuy1KZrIJWQ=='
 headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
 
 req = urllib2.Request(url, body, headers) 
